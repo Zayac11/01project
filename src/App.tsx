3cca9b975e30f456a1af173b3fcfdae918206d8d
Zayac11 <brunogrbstudios@gmail.com>
2021-05-07 16:26:09 +0300

    fixed basename

--- src/App.tsx
+++ src/App.tsx
@@ -70,7 +70,7 @@ let AppContainer = compose<React.ComponentType>(
     connect(mapStateToProps, {initializeApp}))(App);
 
 const SamuraiJSApp: React.FC = () => {
-    return <BrowserRouter>
+    return <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <AppContainer />
         </Provider>
